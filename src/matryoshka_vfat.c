7882d115cc48d33c1577ae2daf099a08d66accef
Aneesh Neelam <neelam.aneesh@gmail.com>
2017-04-21 10:55:42 -0700

    Fixed another static function

--- src/matryoshka_vfat.c
+++ src/matryoshka_vfat.c
@@ -1,7 +1,7 @@
 #include "../include/matryoshka.h"
 #include "../include/matryoshka_vfat.h"
 
-static int vfat_get_free_blocks(struct dm_dev *carrier) {
+int vfat_get_free_blocks(struct dm_dev *carrier) {
   // TODO
 
   return 0;
