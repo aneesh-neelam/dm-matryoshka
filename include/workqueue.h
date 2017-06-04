18f2f40d14275f452389f33db1c59d5f30af2f9e
Aneesh Neelam <neelam.aneesh@gmail.com>
2017-06-04 16:56:15 -0700

    Fixed syntax

--- include/workqueue.h
+++ include/workqueue.h
@@ -5,9 +5,9 @@
 
 #include "target.h"
 
-void wakeup_kmatryoshkad(struct matryoshka_context *)
-void kmatryoshkad_queue_entropy_bio(struct matryoshka_context *, struct bio *)
-struct bio **kmatryoshkad_init_bios(struct bio *, unsigned int)
-inline void kmatryoshkad_init_dev_bio(struct bio *, struct matryoshka_device *, struct io *, bio_end_io_t)
+void wakeup_kmatryoshkad(struct matryoshka_context *);
+void kmatryoshkad_queue_entropy_bio(struct matryoshka_context *, struct bio *);
+struct bio **kmatryoshkad_init_bios(struct bio *, unsigned int);
+inline void kmatryoshkad_init_dev_bio(struct bio *, struct matryoshka_device *, struct io *, bio_end_io_t);
 
 #endif /* WORKQUEUE_H */
