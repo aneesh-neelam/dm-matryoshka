0d7781fc3642e6107eb932b3a1fcddb5dec53708
Aneesh Neelam <neelam.aneesh@gmail.com>
2017-07-03 01:02:11 -0700

    Added function wrapper for CRC32 Little Endian

---
+++ include/integrity.h
@@ -0,0 +1,10 @@
+#ifndef INTEGRITY_H
+#define INTEGRITY_H
+
+#include <linux/types.h>
+
+#define INIT_CRC 0
+
+u32 crc32le(const char *, __kernel_size_t);
+
+#endif /* INTEGRITY_H */
---
+++ src/integrity.c
@@ -0,0 +1,9 @@
+#include <linux/crc32.h>
+#include <kernel/types.h>
+
+#include "../include/integrity.h"
+
+// Calculate CRC32 bitwise little-endian
+u32 crc32le(const char *buf, __kernel_size_t len) {
+  return crc32_le(INIT_CRC, buf, len);
+}
