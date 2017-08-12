2ec3859c6a29fae20d0e9d81ebb893d0f9a7909d
Aneesh Neelam <neelam.aneesh@gmail.com>
2017-08-11 17:02:59 -0700

    Fix syntax 2

--- include/utility.h
+++ include/utility.h
@@ -7,7 +7,7 @@ int convertStringToU8(u8* res, char* str);
 int convertStringToSector_t(sector_t* res, char* str);
 int checkRatio(u8 num_carrier, u8 num_entropy);
 
-struct matryoshka_io *init_matryoshka_io(struct matryoshka_context*, struct bio*)
+struct matryoshka_io *init_matryoshka_io(struct matryoshka_context*, struct bio*);
 void io_accumulate_error(struct matryoshka_io*, int);
 
 struct bio *mybio_clone(struct bio*);
