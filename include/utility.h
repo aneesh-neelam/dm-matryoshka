957dc9e0002afa141b1e1633cdf91b27726ad1c4
Aneesh Neelam <neelam.aneesh@gmail.com>
2017-08-13 14:50:13 -0700

    Proper C header added

--- include/utility.h
+++ include/utility.h
@@ -3,6 +3,8 @@
 
 #include <linux/types.h>
 
+#include <target.h>
+
 int convertStringToU8(u8* res, char* str);
 int convertStringToSector_t(sector_t* res, char* str);
 int checkRatio(u8 num_carrier, u8 num_entropy);
